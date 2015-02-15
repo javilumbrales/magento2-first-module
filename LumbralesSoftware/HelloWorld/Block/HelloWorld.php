fd05b45f52dae94b3c3174c4d2972ffb38d8f2e8
Javier Carrascal <javier.carrascal@fitchlearning.com>
2015-02-15 14:43:54 +0000

    Initial commit

---
+++ LumbralesSoftware/HelloWorld/Block/HelloWorld.php
@@ -0,0 +1,14 @@
+<?php
+namespace LumbralesSoftware\HelloWorld\Block;
+
+/**
+ * HelloWorld
+ *
+ * @package
+ * @version $id$
+ * @copyright 2015, Lumbrales Software
+ * @author Javier Carrascal <info@lumbrales-software.com>
+ */
+class HelloWorld extends \Magento\Framework\View\Element\Template
+{
+}
